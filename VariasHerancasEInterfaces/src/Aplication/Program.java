098ca2d52eb5f2e421999f99cde299928c333e89
Jailine <jay.santana964@gmail.com>
2023-01-30 17:27:22 -0300

    Possível solução para *herança multipla

--- VariasHerancasEInterfaces/src/Aplication/Program.java
+++ VariasHerancasEInterfaces/src/Aplication/Program.java
@@ -1,17 +1,17 @@
 package Aplication;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Printer p = new Printer("1080");
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("My Letter");
 		p.print("My Letter");
 
-		Scanner s = new Scanner("2003");
+		ConcreteScanner s = new ConcreteScanner("2003");
 		s.processDoc("My Email");
 		System.out.println("Scan result: " + s.scan());
 	}
