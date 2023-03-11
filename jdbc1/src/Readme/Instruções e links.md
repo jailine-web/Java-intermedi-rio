625e239a734063b20133c16dbcbadf23890b0abd
Jailine <jay.santana964@gmail.com>
2023-03-10 21:03:13 -0300

    Introdução de conexão com o banco de dados

---
+++ jdbc1/src/Readme/Instruções e links.md
@@ -0,0 +1,20 @@
+#Links:
+Link do banco de dados: https://dev.mysql.com/downloads/workbench/
+Link do conector jdbc: https://dev.mysql.com/downloads/connector/j/?os=26 
+
+#Comandos
+
+Statement é a classe da Api responsável por montar um comando SQL para ser executado(recuperação dos 
+dados num banco de dados)
+
+ResultSet é a classe respresenta um objeto que contém o resultado da consulta em forma de tabela. Além
+disso o objeto resultSet possui operações para posicionar os elementos.
+
+##Operações:
+
+first()
+beforeFirst()
+next()
+absolut(int)
+
+
---
+++ jdbc1/src/db/DB.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+/*
+ * Classe que carrega as propriedades/dados que estão no arquivo db.properties
+*/
+
+public class DB {
+
+	private static Connection con = null;
+	
+	//Realiza a conexão com o banco de dados
+	public static Connection getConexao() {
+		
+		if(con == null) {
+			try {
+				Properties props = carregarPropriedades();
+				String url = props.getProperty("dburl");
+				con = DriverManager.getConnection(url, props);
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return con;
+	}
+	//Método que fecha a conexão
+	
+	public static void fecharConexao() {
+		if(con != null) {
+			try {
+				con.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//Método que carrega os dados do arquivo properties
+	private static Properties carregarPropriedades() {
+		
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties prop = new Properties();
+			prop.load(fs);
+			return prop;
+		}
+		catch(IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
